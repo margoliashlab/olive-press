7a4cfdd4e3cc273e46ae3c93fcea17e787249e48
Kyler Brown <kylerjbrown@gmail.com>
2015-03-18 13:38:53 -0500

    added check on precompressed, installation

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # olive-press
-Copies data in arf file to new file with no chunking and gzip compression.
-Data integrety is checked twice, then the original file is replaced.
+Copies data in an [arf](https://github.com/melizalab/arf) file to new file with no chunking and gzip compression.
+Data integrety is checked, then the original file is replaced, if the `--replace` flag is included.
 
-olive-press improved both the size of data on disk (75% to 40% of original size)  and speed of analysis by removing
+The program `olive-press` improves both the size of data on disk (75% to 40% of original size)  and speed of analysis by removing
 "chunking" in the storage of arrays.
+
+## installation
+`python setup.py install`
+
+
+##usage
+For usage help, type `olive-press -h`
+
 
     usage: unchunk [-h] [-c COMPRESSION] [-v] chunked_arf [chunked_arf ...]
     
@@ -18,3 +26,4 @@ olive-press improved both the size of data on disk (75% to 40% of original size)
                             compression level, 0 = none (fast), 9 = high (slow),
                             default: 9
       -v, --verbose
+      --replace             replaces old arf file with new, compressed arf file
---
+++ setup.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import io
+import os
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*filenames, **kwargs):
+        encoding = kwargs.get('encoding', 'utf-8')
+        sep = kwargs.get('sep', '\n')
+        buf = []
+        for filename in filenames:
+                with io.open(filename, encoding=encoding) as f:
+                        buf.append(f.read())
+                        return sep.join(buf)
+
+long_description = read('README.md')
+
+
+class PyTest(TestCommand):
+        def finalize_options(self):
+                TestCommand.finalize_options(self)
+                self.test_args = []
+                self.test_suite = True
+
+        def run_tests(self):
+                import pytest
+                errcode = pytest.main(self.test_args)
+                sys.exit(errcode)
+
+setup(
+        name='olive-press',
+        scripts=['olive-press'],
+        url='http://github.com/kjbrown/olive-press/',
+        license='GPL License',
+        author='Kyler Brown',
+        version='1.2.1',
+        tests_require=['pytest'],
+        install_requires=['h5py',
+                          'numpy',
+                          'arf>=2.0.0'],
+        dependency_links = ['http://github.com/melizalab/arf/tarball/master#egg=arf-2.2.0'],
+        cmdclass={'test': PyTest},
+        author_email='kylerjbrown@gmail.com',
+        description='arf file compression and unchunking program',
+        long_description=long_description,
+        platforms='any',
+        classifiers=[
+                'Programming Language :: Python',
+                'Development Status :: 4 - Beta',
+                'Natural Language :: English',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: MIT License',
+                'Operating System :: OS Independent',
+                'Topic :: Scientific/Engineering',
+        ],
+    extras_require={
+            'testing': ['pytest'],
+    }
+)
